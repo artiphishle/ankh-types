8ca4d4bc9128047682563aedab6614d1d30b619f
artiphishle <fabio.gartenmann@protonmail.com>
2025-02-18 03:49:15 +0100

    Add nav type 'drawer' & 'tabs'

--- lib/ankh/config.types.ts
+++ lib/ankh/config.types.ts
@@ -2,8 +2,15 @@ import type { IAnkhAuth } from "./auth.types";
 import type { IAnkhPage } from "./page.types";
 import type { IAnkhTheme } from "./theme.types";
 
-/** @interface IAnkhConfig */
+/*** @enum EAnkhNavType */
+export enum EAnkhNavType {
+  Drawer = "drawer",
+  Tabs = "tabs",
+}
+
+/*** @interface IAnkhConfig */
 export interface IAnkhConfig {
+  readonly nav: { type: EAnkhNavType };
   readonly themes: IAnkhTheme[];
   readonly auth: IAnkhAuth;
   readonly pages: IAnkhPage[];
